b2c5929d8dc6adc7a04b007d57165b892360e4fb
abubokor748 <abubokor748@gmail.com>
2024-10-09 15:47:29 +0600

    like button is working

--- script.js
+++ script.js
@@ -22,25 +22,48 @@ const loadCategoriesPets = (category) => {
     .then((data) => displayCards(data.data))
     .catch((error) => console.log(error));
   // alert(samePet);
-  
 };
 
-// show modal 
+// show modal
 const loadModal = (petId) => {
   fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`)
     .then((res) => res.json())
     .then((data) => displayModal(data.petData))
     .catch((error) => console.log(error));
-  	console.log(petId);
-}
+};
+
+const loadImage = (petId) => {
+  fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`)
+    .then((res) => res.json())
+    .then((data) => displayImage(data.petData))
+    .catch((error) => console.log(error));
+};
+
+
+const displayImage = (singleImage) => {
+  const imageContainer = document.getElementById("images");
+
+  const oneImage = document.createElement("div");
+
+  oneImage.innerHTML = `
+    <img
+    src=${singleImage.image}
+    alt="404 error"
+    class="rounded-xl w-full object-cover border">
+  
+  `;
+  imageContainer.appendChild(oneImage);
+
+  document.getElementById("load-image").addEventListener("click", displayImage);
+};
+
 
 // display count down modal details
-
 let countDownFrom = 3;
 let countDownInterval;
 const countModal = () => {
-  document.getElementById("countDownModal")
-  const countDownDisplay = document.getElementById("count-down-display")
+  document.getElementById("countDownModal");
+  const countDownDisplay = document.getElementById("count-down-display");
 
   // modal open
   document.getElementById("modal-countDown-show").click();
@@ -53,14 +76,13 @@ const countModal = () => {
   const countDownInterval = setInterval(() => {
     countDownFrom--;
     countDownDisplay.innerText = countDownFrom;
-    if(countDownFrom === 0){
+    if (countDownFrom === 0) {
       clearInterval(countDownInterval);
       closeModal();
     }
     countDownDisplay.innerText = countDownFrom;
-    
-  }, 1000)
-}
+  }, 1000);
+};
 
 // closing function
 function closeModal() {
@@ -69,57 +91,75 @@ function closeModal() {
 }
 
 // all buttons are together
-const buttons = document.querySelectorAll('.btn-countdown');
-buttons.forEach(button => {
-  button.addEventListener('click', closeModal)
-})
+const buttons = document.querySelectorAll(".btn-countdown");
+buttons.forEach((button) => {
+  button.addEventListener("click", closeModal);
+});
 
 // display modal for details
 const displayModal = (modal) => {
   console.log(modal);
-  const detailContainer = document.getElementById("modal-content")
+  const detailContainer = document.getElementById("modal-content");
 
   document.getElementById("modal-details-show").click();
 
   detailContainer.innerHTML = `
-  <img src=${modal.image} alt="" class="w-full object-cover">
+  <img src=${modal.image} alt="404 error" class="w-full object-cover">
   <h1 class="mt-6 font-bold text-2xl ">${modal.pet_name}</h1>
     <div class="grid grid-cols-2">
       <div class="flex gap-2">
         <img src="./images/breed.png" alt="" class="px-2 py-2">
-        ${modal.breed ? `<p> Breed: ${modal.breed}</p>` : '<p>Breed: Unknown</p>'}
+        ${
+          modal.breed
+            ? `<p> Breed: ${modal.breed}</p>`
+            : "<p>Breed: Unknown</p>"
+        }
       </div>
       <div class="flex gap-2">
         <img src="./images/Birth.png" alt="" class="px-2 py-2 ">
-        ${modal.date_of_birth ? `<p>Date of Birth: ${modal.date_of_birth}</p>` : '<p>Date of Birth: Unknown</p>'}
+        ${
+          modal.date_of_birth
+            ? `<p>Date of Birth: ${modal.date_of_birth}</p>`
+            : "<p>Date of Birth: Unknown</p>"
+        }
       </div>
       <div class="flex gap-2">
         <img src="./images/Gender.png" alt="" class="px-2 py-2">
-        ${modal.gender ? `<p>Gender: ${modal.gender}</p>` : '<p>Gender: Unknown</p>'}
+        ${
+          modal.gender
+            ? `<p>Gender: ${modal.gender}</p>`
+            : "<p>Gender: Unknown</p>"
+        }
       </div>
       <div class="flex gap-2">
         <img src="./images/price.png" alt="" class="px-2 py-2">
-        ${modal.price ? `<p>price: ${modal.price}$</p>` : '<p>price: Inbox </p>'}
+        ${
+          modal.price ? `<p>price: ${modal.price}$</p>` : "<p>price: Inbox </p>"
+        }
       </div>
       <div class="flex gap-2">
         <img src="./images/Gender.png" alt="" class="px-2 py-2">
-        ${modal.vaccinated_status ? `<p>Vaccinated status: ${modal.vaccinated_status}</p>` : '<p>Vaccinated status: Unknown</p>'}
+        ${
+          modal.vaccinated_status
+            ? `<p>Vaccinated status: ${modal.vaccinated_status}</p>`
+            : "<p>Vaccinated status: Unknown</p>"
+        }
       </div>
     </div>
     <hr>
     <h2 class="mt-6 font-semibold text-xl">Details Information</h2>
     <p>${modal.pet_details}</p>
-  `
-} 
+  `;
+  
 
+};
 
 // display cards
 const displayCards = (pets) => {
   const cardsContainer = document.getElementById("cards");
   cardsContainer.innerHTML = "";
 
-  if(pets.length == 0){
-
+  if (pets.length == 0) {
     cardsContainer.classList.remove("grid");
     cardsContainer.innerHTML = `
     <div class="min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
@@ -133,22 +173,19 @@ const displayCards = (pets) => {
     </div>
     `;
     return;
-  }
-  else{
+  } else {
     cardsContainer.classList.add("grid");
   }
-  
-  pets.forEach((pet) => {
-    console.log(pet);
 
-    // create card
+  pets.forEach((pet) => {
+    
     const card = document.createElement("div");
-    card.classList = "card bg-base-100 shadow-xl";
+    card.classList = "card bg-base-100 shadow-xl border";
     card.innerHTML = `
          <figure class="lg:px-10 md:px-8 px-4 pt-10">
     <img
       src=${pet.image}
-      alt="Shoes"
+      alt="404 error"
       class="rounded-xl w-full object-cover">
   </figure>
   <div class="card-body">
@@ -156,33 +193,49 @@ const displayCards = (pets) => {
     <div>
       <div class="flex gap-2">
       <img src="./images/breed.png" alt="" class="px-2 py-2">
-      ${pet.breed ? `<p> Breed: ${pet.breed}</p>` : '<p>Breed: Unknown</p>'}
+      ${pet.breed ? `<p> Breed: ${pet.breed}</p>` : "<p>Breed: Unknown</p>"}
       </div>
       <div class="flex gap-2">
       <img src="./images/Birth.png" alt="" class="px-2 py-2">
-      ${pet.date_of_birth ? `<p>Date of Birth: ${pet.date_of_birth}</p>` : '<p>Date of Birth: Unknown</p>'}
+      ${
+        pet.date_of_birth
+          ? `<p>Date of Birth: ${pet.date_of_birth}</p>`
+          : "<p>Date of Birth: Unknown</p>"
+      }
       </div>
       <div class="flex gap-2">
       <img src="./images/Gender.png" alt="" class="px-2 py-2">
-      ${pet.gender ? `<p>Gender: ${pet.gender}</p>` : '<p>Gender: Unknown</p>'}
+      ${pet.gender ? `<p>Gender: ${pet.gender}</p>` : "<p>Gender: Unknown</p>"}
       </div>
       <div class="flex gap-2">
-      <img src="./images/price.png" alt="" class="px-2 py-2">
-      ${pet.price ? `<p>price: ${pet.price}$</p>` : '<p>price: Inbox </p>'}
+      <img src="./images/price.png" alt="404 error" class="px-2 py-2">
+      ${pet.price ? `<p>price: ${pet.price}$</p>` : "<p>price: Inbox </p>"}
       </div>
     </div>
     <hr>
     <div class="card-actions flex justify-between">
-      <button class="btn bg-white text-[#0E7A81] border">
+
+      <button id="load-image" onclick="loadImage(${pet.petId})" class="btn  bg-white text-[#0E7A81] border">
         <img src="./images/like.png" alt="">
       </button>
-      <button onclick="countModal()" class="btn bg-white text-[#0E7A81] border btn-countdown">Adopt</button>
-      <button onclick="loadModal(${pet.petId})" class="btn bg-white text-[#0E7A81] border">Details</button>
+
+      <button onclick="countModal()" class="btn bg-white text-[#0E7A81] border btn-countdown">
+        Adopt
+      </button>
+
+      <button onclick="loadModal(${
+        pet.petId
+      })" class="btn bg-white text-[#0E7A81] border">
+        Details
+      </button>
     </div>
   </div>
         `;
     cardsContainer.append(card);
+
+    
   });
+
 };
 
 // displayCategories for buttons
@@ -194,11 +247,10 @@ const displayCategories = (categories) => {
 
     // create a button
 
-
     const buttonContainer = document.createElement("div");
-    buttonContainer.innerHTML = 
-    `
-      <button onclick="loadCategoriesPets('${item.category}')" class="btn flex justify-center w-40 h-16">
+
+    buttonContainer.innerHTML = `
+      <button onclick="loadCategoriesPets('${item.category}')" class="btn flex justify-center w-40 h-16 mx-auto">
         <img
       src=${item.category_icon}
       alt="404 Error"
@@ -207,19 +259,16 @@ const displayCategories = (categories) => {
         <p class="font-bold">${item.category}</p>
       </div>
       </button>
-    `
+    `;
     // button.innerText = item.category;
 
     //  add button to category
 
     categoryContainer.append(buttonContainer);
   });
-
 };
 
 loadCategories();
 loadCards();
-
-/* <p> Birth: ${pet.date_of_birth}</p> */
 
 
